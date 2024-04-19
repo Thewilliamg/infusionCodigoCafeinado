d73990c79416b69c479bab54b6ee183d4ce467fe
Thewilliamg <156461507+Thewilliamg@users.noreply.github.com>
2024-04-19 12:58:48 -0500

    feat: :construction: no se que hago, modulacion
    Informacion trabajada en clase

--- js/bill.js
+++ js/bill.js
@@ -1,2 +1,17 @@
 import {example} from "./component/shopBillCamper.js"
-alert(example())
+let myBill = document.querySelector("#myBill")
+let data =await getAllproductBill();
+let row = ""
+data.forEach(products=>{
+    val.products.forEach(product=>{
+        row += `
+            <tr>
+                <td>${product.description}</td>
+                <td>${product.quantity}</td>
+                <td>${product.price}</td>
+                <td>${product.total}</td>
+            </tr>
+            `
+    })
+})
+myBill.innerHTML = row
--- js/component/shopBillCamper.js
+++ js/component/shopBillCamper.js
@@ -1,1 +1,6 @@
-export const example = () => "Hello world"
+export const getAllproductBill = async() => 
+{
+    let conexion = await fetch("http://localhost:5600/camper")
+    let data = await conexion.json();
+    return data;
+} 
